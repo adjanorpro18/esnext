43e0481bf0be67fa53864de965203ee3eaa9ce4e
Enyon ADJANOR <enyonadjanor@gmail.com>
2021-07-19 14:10:31 +0200

    let + const

---
+++ app.js
@@ -0,0 +1,20 @@
+//Creation de la variable favoriteCityId avec le mot cle let
+
+let favoriteCityId = "rome"
+
+console.log(favoriteCityId); //
+
+//Affectation d'une nouvelle valeur à la variable favoriteCityId 
+
+let favoriteCityId = "paris"
+console.log(favoriteCityId);
+
+
+//Creation d'une constante CitiesId avec le mot cle const
+
+const citiesId = ["paris", "nyc", "rome", "rio-de-janeiro"]
+
+console.log(citiesId);
+
+//Affectation d'une valeur para exemple tableau vide à la const CitiesId
+const citiesId = []
