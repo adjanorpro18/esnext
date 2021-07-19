1ae9e7bf8c9bd92f745c7b5c7ae5fe4c8de4e016
Enyon ADJANOR <enyonadjanor@gmail.com>
2021-07-19 17:58:14 +0200

    classes

--- app.js
+++ app.js
@@ -59,3 +59,88 @@ const [parisId, nycId, ...othersCitiesId] = citiesId;
 console.log(parisId);
 console.log(nycId);
 console.log(othersCitiesId);
+
+
+// Classe Trip
+
+class Trip {
+    constructor(id, name, imageUrl, price) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.price = price;
+    }
+
+    //Je renvoi mon toString
+    toString() {
+        return this.id + ' ' + this.name + ' ' + this.imageUrl
+    }
+
+    // Setter sur la variable this._id de l'attribut id
+    set id(monId) {
+        this._id = monId
+    }
+
+    // Getter sur la variable this._id de l'attribut id
+    get id() {
+        return this._id
+    }
+
+    // Setter sur la variable this._name de l'attribut name
+    set name(monName) {
+        this._name = monName
+    }
+
+    // Getter sur la variable this._name de l'attribut name
+    get name() {
+        return this._name
+    }
+
+    // Setter sur la variable this._imageUrl de l'attribut imageUrl
+    set imageUrl(monImageUrl) {
+        this._imageUrl = monImageUrl
+    }
+
+    // Getter sur la variable this._imageUrl de l'attribut imageUrl
+    get imageUrl() {
+        return this._imageUrl
+    }
+
+    // Setter sur la variable this._pricede l'attribut price
+    set price(monPrice) {
+        this._price = monPrice
+    }
+
+    // Getter sur la variable this._price de l'attribut price
+    get price() {
+        return this._price
+    }
+
+    // Ajouter une methode getDefaultTrip
+    static getDefaultTrip() {
+
+        return {
+            id: 'rio-de-Janeiro',
+            name: 'Rio de Janeiro',
+            imageUrl: 'img/rio-de-janeiro.jpg'
+        }
+    }
+
+}
+
+//creation d'un objet parisTrip
+
+let parisTrip = new Trip('paris', 'Paris', 'img/paris.jpg', 100)
+
+console.log(parisTrip);
+
+//avec l'ajout de la methode toString()
+
+console.log(parisTrip.toString());
+
+
+//Constante utilisant la methode getDefaultTrip de la classe 
+
+const defaultTrip = Trip.getDefaultTrip();
+
+console.log(defaultTrip.toString());
